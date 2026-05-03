f7efad6ba97fe425b31f69ded62217c0b56e1a49
sreeramlal VP <sreeramlalvp123@gmail.com>
2026-05-03 12:14:10 +0530

    updates made

--- js/components.js
+++ js/components.js
@@ -65,62 +65,116 @@ $(document).ready(function () {
                     </div>
                 </div>
             </div>
-            <section class="portfolio spad pt-0">
-                <div class="container">
-                    <div class="row portfolio__gallery no-gutters">
-                        <div class="col-lg-4 col-md-6 col-sm-6 mix digital-marketing">
-                            <div class="portfolio__item">
-                                <div class="portfolio__item__video set-bg" data-setbg="img/portfolio/phonepay.png">
-                                    <a href="https://m.youtube.com/watch?v=kfiAp_Hq1eM" class="play-btn video-popup"><i
-                                            class="fa fa-play"></i></a>
-                                </div> 
+        </div>
+        <style>
+            .top-works-scroll {
+                display: flex;
+                flex-direction: column;
+                overflow-x: auto;
+                overflow-y: hidden;
+                gap: 0;
+                margin: 0;
+                padding: 0;
+                scrollbar-width: none; /* Firefox */
+                -ms-overflow-style: none;  /* IE and Edge */
+            }
+            .top-works-scroll::-webkit-scrollbar {
+                display: none; /* Chrome, Safari, Opera */
+            }
+            .scroll-row {
+                display: flex;
+                flex-direction: row;
+                flex-wrap: nowrap;
+                margin: 0; padding: 0; gap: 0;
+            }
+            .creative-img {
+                height: 240px;
+                width: auto;
+                display: block;
+                margin: 0; padding: 0; gap: 0;
+                flex-shrink: 0;
+            }
+            .video-item {
+                position: relative;
+                display: inline-flex;
+                flex-shrink: 0;
+                height: 240px;
+                margin: 0; padding: 0; gap: 0;
+            }
+            .video-overlay {
+                position: absolute;
+                top: 0; left: 0;
+                width: 100%; height: 100%;
+                background: transparent;
+                margin: 0 !important;
+            }
+        </style>
+        <section class="portfolio pt-0 pb-0" style="margin: 0; padding: 0;">
+            <div class="container-fluid" style="padding: 0;">
+                <div class="portfolio__gallery top-works-scroll pb-0 mb-0">
+                    <div class="scroll-row">
+                        <div class="video-item mix digital-marketing">
+                            <img src="img/portfolio/phonepay.png" class="creative-img">
+                            <div class="portfolio__item__video video-overlay">
+                                <a href="https://m.youtube.com/watch?v=kfiAp_Hq1eM" class="play-btn video-popup"><i class="fa fa-play"></i></a>
                             </div>
                         </div>
-                        <div class="col-lg-4 col-md-6 col-sm-6 mix branding">
-                            <div class="portfolio__item">
-                                <div class="portfolio__item__video set-bg" data-setbg="img/portfolio/kappiri.png
-                                ">
-                                    <a href="https://youtu.be/FXr-2BpS4Po?si=4ENMvQX2Tk3CHRfu" class="play-btn video-popup"><i
-                                            class="fa fa-play"></i></a>
-                                </div>
+                        <div class="video-item mix branding">
+                            <img src="img/portfolio/kappiri.png" class="creative-img">
+                            <div class="portfolio__item__video video-overlay">
+                                <a href="https://youtu.be/FXr-2BpS4Po?si=4ENMvQX2Tk3CHRfu" class="play-btn video-popup"><i class="fa fa-play"></i></a>
                             </div>
                         </div>
-                        <div class="col-lg-4 col-md-6 col-sm-6 mix digital-marketing">
-                            <div class="portfolio__item">
-                                <div class="portfolio__item__video set-bg" data-setbg="img/portfolio/paperboat.png">
-                                    <a href="https://m.youtube.com/watch?v=5XdWwAL--mQ" class="play-btn video-popup"><i
-                                            class="fa fa-play"></i></a>
-                                </div>
+                        <div class="video-item mix digital-marketing">
+                            <img src="img/portfolio/paperboat.png" class="creative-img">
+                            <div class="portfolio__item__video video-overlay">
+                                <a href="https://m.youtube.com/watch?v=5XdWwAL--mQ" class="play-btn video-popup"><i class="fa fa-play"></i></a>
                             </div>
                         </div>
-                        <div class="col-lg-4 col-md-6 col-sm-6 mix digital-marketing">
-                            <div class="portfolio__item">
-                                <div class="portfolio__item__video set-bg" data-setbg="img/portfolio/gopro.png">
-                                    <a href="https://youtu.be/TmDsi-9pgz8?si=EwDJHVq_ICLJlFHM" class="play-btn video-popup"><i
-                                        class="fa fa-play"></i></a>
-                                </div>
+
+                        <img src="img/top-works-creative/DSC00188.JPG" class="creative-img mix creatives">
+                        <img src="img/top-works-creative/DSC06462.JPG" class="creative-img mix creatives">
+                        <img src="img/top-works-creative/DSC07960.JPG" class="creative-img mix creatives">
+                        <img src="img/top-works-creative/DSC08116.JPG" class="creative-img mix creatives">
+                        <img src="img/top-works-creative/DSC08184.JPG" class="creative-img mix creatives">
+                        <img src="img/top-works-creative/DSC08262.JPG" class="creative-img mix creatives">
+                        <img src="img/top-works-creative/DSC08764.jpg" class="creative-img mix creatives">
+                    </div>
+                    
+                    <div class="scroll-row">
+                        <div class="video-item mix digital-marketing">
+                            <img src="img/portfolio/gopro.png" class="creative-img">
+                            <div class="portfolio__item__video video-overlay">
+                                <a href="https://youtu.be/TmDsi-9pgz8?si=EwDJHVq_ICLJlFHM" class="play-btn video-popup"><i class="fa fa-play"></i></a>
                             </div>
                         </div>
-                        <div class="col-lg-4 col-md-6 col-sm-6 mix digital-marketing">
-                            <div class="portfolio__item">
-                                <div class="portfolio__item__video set-bg" data-setbg="img/portfolio/gemini.png">
-                                    <a href="https://youtu.be/UJscUDJCq1s?si=STrGlUlrOn9CnulO" class="play-btn video-popup"><i
-                                        class="fa fa-play"></i></a>
-                                </div>
+                        <div class="video-item mix digital-marketing">
+                            <img src="img/portfolio/gemini.png" class="creative-img">
+                            <div class="portfolio__item__video video-overlay">
+                                <a href="https://youtu.be/UJscUDJCq1s?si=STrGlUlrOn9CnulO" class="play-btn video-popup"><i class="fa fa-play"></i></a>
                             </div>
                         </div>
-                        <div class="col-lg-4 col-md-6 col-sm-6 mix digital-marketing">
-                            <div class="portfolio__item">
-                                <div class="portfolio__item__video set-bg" data-setbg="img/portfolio/foodadvfx.png">
-                                    <a href="https://youtu.be/ECdSHubQGi8?si=CpiKRRDoNOVTuQmn" class="play-btn video-popup"><i
-                                            class="fa fa-play"></i></a>
-                                </div> 
+                        <div class="video-item mix digital-marketing">
+                            <img src="img/portfolio/foodadvfx.png" class="creative-img">
+                            <div class="portfolio__item__video video-overlay">
+                                <a href="https://youtu.be/ECdSHubQGi8?si=CpiKRRDoNOVTuQmn" class="play-btn video-popup"><i class="fa fa-play"></i></a>
                             </div>
                         </div>
+
+                        <img src="img/top-works-creative/DSC08804.JPG" class="creative-img mix creatives">
+                        <img src="img/top-works-creative/IMG_3768.JPG" class="creative-img mix creatives">
+                        <img src="img/top-works-creative/IMG_6152.JPG" class="creative-img mix creatives">
+                        <img src="img/top-works-creative/IMG_7282.JPG" class="creative-img mix creatives">
+                        <img src="img/top-works-creative/IMG_7863.JPG" class="creative-img mix creatives">
+                        <img src="img/top-works-creative/IMG_9232.JPG" class="creative-img mix creatives">
+                        <img src="img/top-works-creative/IMG_9845.JPG" class="creative-img mix creatives">
+                        <img src="img/top-works-creative/ld-export-2135895f-09012025_SnapseedCopy.jpg" class="creative-img mix creatives">
+                        <img src="img/top-works-creative/DSC08465_SnapseedCopy.jpg" class="creative-img mix creatives">
+                        <img src="img/top-works-creative/DSC08767.jpg" class="creative-img mix creatives">
                     </div>
                 </div>
-            </section>
-        </div>
+            </div>
+        </section>
     `;
 
     // Inject the component into placeholders
